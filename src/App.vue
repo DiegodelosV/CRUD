ca87282bd9369b913b0b6e8555c8961dfa2eca41
DiegodelosV <diegodelosv@gmail.com>
2024-09-13 16:23:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import colaboradores from './components/Colaboradores.vue';
+</script>
+
+<template>
+  <colaboradores />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA59bJsJf770ynbJcffmjRNNH7fFFcQ_xU",
+  authDomain: "crud-colaboradores-5916d.firebaseapp.com",
+  projectId: "crud-colaboradores-5916d",
+  storageBucket: "crud-colaboradores-5916d.appspot.com",
+  messagingSenderId: "1011001117128",
+  appId: "1:1011001117128:web:2faaa2012bb7e636ee18fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
